b47eb4475eccf34b1065b429d601d4559b5ddb92
Yerim159 <78583768+EomYerim@users.noreply.github.com>
2024-09-20 18:46:55 +0900

    Title: Carry 피하기, Time: 366ms, Memory: 25MB, Status: Passed - CodeTree

--- 240920/Carry 피하기/escaping-carry.py
+++ 240920/Carry 피하기/escaping-carry.py
@@ -3,31 +3,26 @@ from itertools import combinations
 
 input = sys.stdin.readline
 
+# carry인지 체크
+def is_carry_free(numbers):
+    digit_sums = [0] * max(len(str(num)) for num in numbers)
+    for num in numbers:
+        for i, digit in enumerate(str(num)[::-1]):
+            digit_sums[i] += int(digit)
+            if digit_sums[i] >= 10: # 각 자릿수의 합이 10 이상이 되는지 체크
+                return False
+    return True
+
 N = int(input())
 numbers = [int(input().strip()) for _ in range(N)]
 
-max_length = len(str(max(numbers)))
-
-numbers_str = [str(num).zfill(max_length) for num in numbers]
-
-# carry가 발생하지 않는 조합인지 확인하는 함수
-def is_valid_combination(num1, num2):
-    for k in range(max_length):
-        if int(num1[k]) + int(num2[k]) >= 10:
-            return False
-    return True
-
-max_count = 1  # 최소 하나의 숫자는 항상 유효하므로 1로 초기화
-
-# 2개 이상의 숫자 조합을 비교
-for r in range(2, N + 1):  # 2개부터 N개까지 조합
-    for x in combinations(numbers_str, r):
-        valid = True
-        for num1, num2 in combinations(x, 2):
-            if not is_valid_combination(num1, num2):
-                valid = False
-                break
-        if valid:
-            max_count = max(max_count, r)
+max_count = 1
+for r in range(2, N + 1):
+    for combo in combinations(numbers, r):
+        if is_carry_free(combo):
+            max_count = r
+            break
+    else:
+        break
 
 print(max_count)
