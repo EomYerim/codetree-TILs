1b823269e00f5407d43e66357a7e097925ac06d0
Yerim159 <78583768+EomYerim@users.noreply.github.com>
2024-09-28 19:26:21 +0900

    Title: 빙산의 일각, Time: 5037ms, Memory: 26MB, Status: Time Limit Exceed - CodeTree

---
+++ 240928/빙산의 일각/the-tip-of-the-iceberg.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+N=int(input())
+height = [int(input()) for _ in range(N)]
+res=[]
+for i in range(N): #
+    ans=0
+    h=height[i]
+    flag=False
+    for j in range(N):
+        if(height[j]-h>0):
+            if(flag==False):
+                ans+=1
+            flag=True
+        else:
+            if(flag==True):
+                flag=False
+    res.append(ans)
+print(max(res))
