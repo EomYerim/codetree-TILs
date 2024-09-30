325595fe5458d8d8af19b70c1456ae555f1cd206
Yerim159 <78583768+EomYerim@users.noreply.github.com>
2024-09-30 13:23:22 +0900

    Title: 빙산의 일각, Time: 160ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240930/빙산의 일각/the-tip-of-the-iceberg.py
+++ 240930/빙산의 일각/the-tip-of-the-iceberg.py
@@ -1,20 +1,34 @@
 import sys
-input = sys.stdin.readline
-N=int(input())
-height = [int(input()) for _ in range(N)]
-res=[0]*N
+from collections import defaultdict
 
-for i in range(N): 
-    ans=0
-    h=height[i]
-    flag=False
-    for j in range(N):
-        if(height[j]-h>0):
-            if(flag==False):
-                ans+=1
-            flag=True
-        else:
-            if(flag==True):
-                flag=False
-    res.append(ans)
-print(max(res))
+input = sys.stdin.readline
+
+def max_iceberg_chunks(N, heights):
+    # 높이별로 인덱스를 저장
+    height_indices = defaultdict(list)
+    for i, h in enumerate(heights):
+        height_indices[h].append(i)
+    
+    # 높이를 오름차순으로 정렬
+    sorted_heights = sorted(set(heights))
+    
+    max_chunks = 0
+    chunks = 0
+    last_index = -2  # 이전 빙산의 인덱스
+    
+    for h in sorted_heights:
+        for i in height_indices[h]:
+            if i > last_index + 1:
+                # 새로운 덩어리 시작
+                chunks += 1
+            last_index = i
+        max_chunks = max(max_chunks, chunks)
+    
+    return max_chunks
+
+# 입력 처리
+N = int(input())
+heights = [int(input()) for _ in range(N)]
+
+# 결과 출력
+print(max_iceberg_chunks(N, heights))
