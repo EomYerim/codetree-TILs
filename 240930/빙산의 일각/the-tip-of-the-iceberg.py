eb6f928a9d4854179f4cd6eef5e581de010ce7ed
Yerim159 <78583768+EomYerim@users.noreply.github.com>
2024-09-30 13:32:13 +0900

    Title: 빙산의 일각, Time: 5088ms, Memory: 25MB, Status: Time Limit Exceed - CodeTree

--- 240930/빙산의 일각/the-tip-of-the-iceberg.py
+++ 240930/빙산의 일각/the-tip-of-the-iceberg.py
@@ -1,34 +1,32 @@
 import sys
-from collections import defaultdict
 
 input = sys.stdin.readline
 
-def max_iceberg_chunks(N, heights):
-    # 높이별로 인덱스를 저장
-    height_indices = defaultdict(list)
-    for i, h in enumerate(heights):
-        height_indices[h].append(i)
-    
-    # 높이를 오름차순으로 정렬
-    sorted_heights = sorted(set(heights))
-    
-    max_chunks = 0
-    chunks = 0
-    last_index = -2  # 이전 빙산의 인덱스
-    
-    for h in sorted_heights:
-        for i in height_indices[h]:
-            if i > last_index + 1:
-                # 새로운 덩어리 시작
-                chunks += 1
-            last_index = i
-        max_chunks = max(max_chunks, chunks)
-    
-    return max_chunks
-
-# 입력 처리
+# n 입력
 N = int(input())
-heights = [int(input()) for _ in range(N)]
 
-# 결과 출력
-print(max_iceberg_chunks(N, heights))
+# height 입력
+height = [int(input()) for _ in range(N)]
+
+# simulate 함수
+def simulate(curr_height):
+    curr_cnt = 0
+    # 맨 왼쪽은 떠있으면
+    if height[0] > curr_height:
+        curr_cnt += 1
+    # 이후의 height를 돌면서
+    for i in range(1, N):
+        # 전 인덱스 값보다 클 때
+        if height[i-1] <= curr_height and height[i] > curr_height:
+            curr_cnt += 1
+    return curr_cnt
+
+max_cnt = 0
+max_height = max(height)
+
+# 완전 탐색 
+for i in range(max_height + 1):
+    max_cnt = max(max_cnt, simulate(i))
+
+# 출력
+print(max_cnt)
