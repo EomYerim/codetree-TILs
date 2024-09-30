320a87b40e540f543775b89e9eb7e9cc09caeed0
Yerim159 <78583768+EomYerim@users.noreply.github.com>
2024-09-30 13:21:59 +0900

    Title: 빙산의 일각, Time: 5009ms, Memory: 27MB, Status: Time Limit Exceed - CodeTree

--- 240930/빙산의 일각/the-tip-of-the-iceberg.py
+++ 240930/빙산의 일각/the-tip-of-the-iceberg.py
@@ -1,34 +1,20 @@
-def count_connected_components(heights, water_level): # DFS를 통해 연결된 빙산의 덩어리 수 계산 
-    N = len(heights)
-    visited = [False] * N
-    count = 0
-    
-    def dfs(i):
-        visited[i] = True
-        # 오른쪽으로 연결된 빙산 탐색
-        if i + 1 < N and not visited[i + 1] and heights[i + 1] > water_level:
-            dfs(i + 1)
-    
-    for i in range(N):
-        if not visited[i] and heights[i] > water_level:
-            dfs(i)
-            count += 1
-    
-    return count
+import sys
+input = sys.stdin.readline
+N=int(input())
+height = [int(input()) for _ in range(N)]
+res=[0]*N
 
-def max_iceberg_chunks(heights):
-    unique_heights = sorted(set(heights))
-    max_chunks = 0
-    
-    for water_level in unique_heights:
-        chunks = count_connected_components(heights, water_level - 1)
-        max_chunks = max(max_chunks, chunks)
-    
-    return max_chunks
-
-# 입력 처리
-N = int(input())
-heights = [int(input()) for _ in range(N)]
-
-# 결과 출력
-print(max_iceberg_chunks(heights))
+for i in range(N): 
+    ans=0
+    h=height[i]
+    flag=False
+    for j in range(N):
+        if(height[j]-h>0):
+            if(flag==False):
+                ans+=1
+            flag=True
+        else:
+            if(flag==True):
+                flag=False
+    res.append(ans)
+print(max(res))
