8540b9a67ae024555df9e0984da7e735fec6c7a1
Yerim159 <78583768+EomYerim@users.noreply.github.com>
2024-11-06 15:51:46 +0900

    Title: 달에서 무게 구하기, Time: 104ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 241106/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int x=13;
+        double percent=0.165;
+
+       System.out.printf("%d * %.6f = %.6f", x, percent, x * percent);
+    }
+}
