3c24f62c57517888021265d1e2e6bcc2df71d848
Yerim159 <78583768+EomYerim@users.noreply.github.com>
2024-11-06 16:12:35 +0900

    Title: 변수 값 교체하기 3, Time: 95ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 241106/변수 값 교체하기 3/replacing-variable-values-3.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int a=3,b=5;
+        int temp;
+        temp=a; //3
+        a=b; //5
+        b=temp;//3
+        System.out.println(a);
+        System.out.println(b);
+        
+    }
+}
